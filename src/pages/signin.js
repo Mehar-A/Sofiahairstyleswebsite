3fc4a50dee0dd72a92d109b041c37a8eabfd0e9f
Mehar <37940855+Mehar-A@users.noreply.github.com>
2021-12-11 17:08:39 -0800

    Sign_page

---
+++ src/pages/signin.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SiginPage = () => {
+
+      return (
+          <div>
+              <h2>Sigin page</h2>
+          </div>          
+      )
+  }
+  
+  export default SiginPage
